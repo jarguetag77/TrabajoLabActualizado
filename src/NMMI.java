11fe8b192e6e82840e302e98af47ad409fcab84a
jarguetag77 <javier.argueta998@gmail.com>
2019-02-22 11:15:52 -0600

    total

--- src/NMMI.java
+++ src/NMMI.java
@@ -249,10 +249,7 @@ public class NMMI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-//PRUEBA DE ACTUALIZACION 
-//PRUEBA DEFINITIVA
-//PRUEBA
-//prueba pull
+
         
      
         jButton4.setVisible(false);
