f4496d22f7fa85d2284aedddf04276ac2c200f82
jarguetag77 <javier.argueta998@gmail.com>
2019-02-22 10:44:10 -0600

    act 2

--- src/NMMI.java
+++ src/NMMI.java
@@ -264,6 +264,7 @@ public class NMMI extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
 //PRUEBA DE ACTUALIZACION 
+//PRUEBA DEFINITIVA
         
         
      
