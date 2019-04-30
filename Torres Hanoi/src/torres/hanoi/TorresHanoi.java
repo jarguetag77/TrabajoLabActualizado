ee9ba53eb901867265dd408711a26415a808794f
U. Mariano Gálvez <alumno@labs.academico>
2019-04-30 11:39:46 -0600

    Torres de Hanoi

---
+++ Torres Hanoi/src/torres/hanoi/TorresHanoi.java
@@ -0,0 +1,16 @@
+
+package torres.hanoi;
+
+
+public class TorresHanoi {
+
+    public static void main(String[] args) {
+        
+        System.out.print("ahashgasdga");
+        
+        
+        
+        
+    }
+    
+}
