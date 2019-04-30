6118b988964b7ce82b97ac9936e13a106a9481dd
U. Mariano Gálvez <alumno@labs.academico>
2019-04-30 11:22:08 -0600

    PULIR PILAS

--- Pilas/Pilas-y-Colas-master/ListasEnlazadas/src/ListasEnlazadas/Inicio.java
+++ Pilas/Pilas-y-Colas-master/ListasEnlazadas/src/ListasEnlazadas/Inicio.java
@@ -13,34 +13,46 @@ public class Inicio {
       Scanner scn = new Scanner(System.in);  
       System.out.println("Ingrese tamaño de la pila:\t");
       int tamaño= scn.nextInt();
+      int i=0, dato=0,op;
+      ListaPila pila = new ListaPila(tamaño);
       
-      ListaPila pila = new ListaPila(tamaño); 
       
-    
+      //ingresar en pila
+      
       do{
+          System.out.print("\nIngrese dato #"+(i+1)+" en la pila:\t\t");
+          dato= scn.nextInt();
+          pila.push(dato);
           
-           System.out.println("\n\nQue desea hacer con la pila?\n\n1.Push\n2.Mostrar");
-      int op= scn.nextInt();
-      switch(op){
-          
-          case 1:{
-              System.out.println("Ingrese tamaño de la pila:\t");
-              int dato= scn.nextInt();
-              
-              pila.push(dato);
-          }
-          
-          case 2:{
-                while (!pila.vacio()) {
-         int value = pila.pop();
-         System.out.print(value);
-         System.out.print(" ");
-      }
-          }
-      }
+          i+=1;
+      }while(i!=tamaño);
       
-      }while(op!=0);   
+      //mostrar pila
+      
+         System.out.print("\nPILA\n");
+      while (!pila.vacio()) {
+         int value = pila.pop();
+         System.out.print(" { "+value+" } ");
+      }
+     
+      do{
+          System.out.print("\n\n\n\n\n1.Eliminar\n2.Mostrar\n\n\t\t\t\t\t");
+          op =scn.nextInt();
+          switch(op){
+              case 1: {
+                  pila.pop();
+              }
+              
+              case 2: {
+                  System.out.print("\nPILA\n");
+                  while (!pila.vacio()) {
+                    int value = pila.pop();
+                    System.out.print(" { "+value+" } ");
+                 }
+              }
+          }
           
+      }while(op!=0);
           
       
       
